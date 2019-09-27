45290cb61b261446aacbf973eccb8723e92917bf
Jhohann Andres Giraldo Jimenez <jgiraldo@UCO.EDU.CO>
2019-09-27 16:54:36 -0500

    Entidades y seed a la base de datos

---
+++ MyVet/MyVet.Web/Migrations/20190927191053_secondaryDB.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyVet.Web.Migrations
+{
+    public partial class secondaryDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FixedPhone",
+                table: "Owners",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Document",
+                table: "Owners",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CellPhone",
+                table: "Owners",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Owners",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FixedPhone",
+                table: "Owners",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Document",
+                table: "Owners",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CellPhone",
+                table: "Owners",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Owners",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
